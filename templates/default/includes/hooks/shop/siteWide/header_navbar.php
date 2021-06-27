252983def09772f4bdfe169f167301265484c7f2
beppe34 <beppe34@users.noreply.github.com>
2021-06-27 16:49:15 +0200

    Update header_navbar.php

--- templates/default/includes/hooks/shop/siteWide/header_navbar.php
+++ templates/default/includes/hooks/shop/siteWide/header_navbar.php
@@ -59,27 +59,8 @@ $(function() {
     });
 });
 
-// for hover
-$('.dropdown-hover').on('mouseenter',function() {
-  if(!$(this).hasClass('show')){
-    $('>[data-toggle="dropdown"]', this).dropdown('toggle');
-  }
-});
-$('.dropdown-hover').on('mouseleave',function() {
-  if($(this).hasClass('show')){
-    $('>[data-toggle="dropdown"]', this).dropdown('toggle');
-  }
-});
-$('.dropdown-hover-all').on('mouseenter', '.dropdown', function() {
-  if(!$(this).hasClass('show')){
-    $('>[data-toggle="dropdown"]', this).dropdown('toggle');
-  }
-});
-$('.dropdown-hover-all').on('mouseleave', '.dropdown', function() {
-  if($(this).hasClass('show')){
-    $('>[data-toggle="dropdown"]', this).dropdown('toggle');
-  }
-});              
+// removed hover -- only click or keyboard navigation for submenus is better!
+
     </script>
 code;
     return $display;
